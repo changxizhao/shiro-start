de412b3ee4bed5a86d0e9b664c89177cd2c3808d
changxizhao <cxz_1120@163.com>
2020-07-02 22:02:35 +0800

    demo用到的数据库文件

---
+++ security-server.sql
@@ -0,0 +1,110 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : virtual_centos
+ Source Server Type    : MySQL
+ Source Server Version : 50729
+ Source Host           : 192.168.0.134:13306
+ Source Schema         : security-server
+
+ Target Server Type    : MySQL
+ Target Server Version : 50729
+ File Encoding         : 65001
+
+ Date: 02/07/2020 22:01:08
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission`  (
+  `pid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `pname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`pid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of permission
+-- ----------------------------
+INSERT INTO `permission` VALUES (1, 'add', NULL);
+INSERT INTO `permission` VALUES (2, 'delete', NULL);
+INSERT INTO `permission` VALUES (3, 'edit', NULL);
+INSERT INTO `permission` VALUES (4, 'query', NULL);
+
+-- ----------------------------
+-- Table structure for permission_role
+-- ----------------------------
+DROP TABLE IF EXISTS `permission_role`;
+CREATE TABLE `permission_role`  (
+  `rid` bigint(20) NOT NULL,
+  `pid` bigint(20) NOT NULL,
+  INDEX `idx_rid`(`rid`) USING BTREE,
+  INDEX `idx_pid`(`pid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of permission_role
+-- ----------------------------
+INSERT INTO `permission_role` VALUES (1, 1);
+INSERT INTO `permission_role` VALUES (1, 2);
+INSERT INTO `permission_role` VALUES (1, 3);
+INSERT INTO `permission_role` VALUES (1, 4);
+INSERT INTO `permission_role` VALUES (2, 1);
+INSERT INTO `permission_role` VALUES (2, 4);
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `rid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `rname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`rid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES (1, 'admin');
+INSERT INTO `role` VALUES (2, 'customer');
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES (1, 'admin', '123');
+INSERT INTO `sys_user` VALUES (2, 'user', '123');
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role`  (
+  `id` bigint(20) NOT NULL,
+  `rid` bigint(20) NOT NULL,
+  INDEX `idx_id`(`id`) USING BTREE,
+  INDEX `idx_rid`(`rid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES (1, 1);
+INSERT INTO `user_role` VALUES (2, 2);
+
+SET FOREIGN_KEY_CHECKS = 1;
